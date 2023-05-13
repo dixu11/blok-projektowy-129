5b555d349245894cb48c1ce5c75afa28611b43f2
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-05-13 13:00:16 +0200

    saving file

--- src/main/java/com/sda/blokprojektowy129/request/ProductRequest.java
+++ src/main/java/com/sda/blokprojektowy129/request/ProductRequest.java
@@ -1,8 +1,15 @@
 package com.sda.blokprojektowy129.request;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class ProductRequest {
     private String name;
     private double price;
+    private MultipartFile file;
+
+    public void setFile(MultipartFile file) {
+        this.file = file;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -18,6 +25,10 @@ public class ProductRequest {
 
     public double getPrice() {
         return price;
+    }
+
+    public MultipartFile getFile() {
+        return file;
     }
 
     @Override
--- src/main/java/com/sda/blokprojektowy129/service/ProductService.java
+++ src/main/java/com/sda/blokprojektowy129/service/ProductService.java
@@ -5,8 +5,14 @@ import com.sda.blokprojektowy129.repository.ProductRepository;
 import com.sda.blokprojektowy129.request.ProductRequest;
 import com.sda.blokprojektowy129.response.ProductResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -14,12 +20,37 @@ import java.util.List;
 public class ProductService {
 
     private ProductRepository productRepository;
+    @Autowired
+    private Environment environment;
 
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     public void addProduct(ProductRequest request) {
+        MultipartFile file = request.getFile();
+        System.out.println(request.getFile().getName());
+        System.out.println(request.getFile().getContentType());
+        System.out.println(request.getFile().getSize());
+        System.out.println(request.getFile().getOriginalFilename());
+
+        if (file.isEmpty()) {
+            throw new IllegalArgumentException("Nie podano pliku!");
+        }
+
+        try {
+            String uploadDirectory = environment.getProperty("spring.servlet.multipart.location");
+            System.out.println(file.getOriginalFilename());
+            String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+            System.out.println(fileName);
+            File destinationFile = new File(uploadDirectory + File.separator + fileName);
+            file.transferTo(destinationFile);
+        } catch (IOException e) {
+            // Obsługa błędu zapisu pliku
+            e.printStackTrace();
+        }
+
+
         //validacja
         Product product = new Product(request.getName(), request.getPrice());
         productRepository.save(product);
